4e8fc1d1bd596d0071c1bf7128f1e11fa7f19e5d
neostetic <gg.polacek@gmail.com>
2023-01-08 15:42:29 +0100

    Added more CSS and JS

--- script/pixelbite.js
+++ script/pixelbite.js
@@ -1,4 +1,5 @@
 const class_library = [
+    ['c', 'color'],
     ['p', 'padding'],
     ['pt','padding-top'],
     ['pd', 'padding-down'],
@@ -10,10 +11,25 @@ const class_library = [
     ['ml', 'margin-left'],
     ['mr', 'margin-right'],
     ['g', 'gap'],
+    ['d', 'display'],
     ['w', 'width'],
     ['h', 'height'],
+    ['border', 'border'],
+    ['bc', 'border-color'],
+    ['br', 'border-radius'],
     ['bg', 'background'],
+    ['ta', 'text-align'],
+    ['fs', 'font-size'],
+    ['fw', 'font-weight'],
+    ['of', 'overflow'],
     ['flex', 'flex'],
+    ['flexDirection', 'flex-direction'],
+    ['flexWrap', 'flex-wrap'],
+    ['flexItems', 'flex-align-items'],
+    ['flexContent', 'flex-align-content'],
+    ['gridColumns', 'grid-template-columns'],
+    ['gridRows', 'grid-template-rows'],
+    ['shadow', 'box-shadow'],
 ]
 
 const styleColor = (propertyValue) => {return getComputedStyle(document.documentElement).getPropertyValue(propertyValue)}
@@ -27,7 +43,53 @@ const color_library = [
     ['info', styleColor('--info-color')],
 ]
 
+let color_library_hsl = [
+    ['white', 0, '0%'],
+    ['red', 0, '100%'],
+    ['orange', 36, '100%'],
+    ['yellow', 59, '100%'],
+    ['greenyellow', 74, '100%'],
+    ['green', 108, '100%'],
+    ['teal', 154, '100%'],
+    ['cyan', 182, '100%'],
+    ['blue', 235, '100%'],
+    ['purple', 275, '100%'],
+    ['pink', 300, '100%'],
+]
+
+const addColorHsl = (name, hue, saturation) => {
+        if (saturation.toString().includes('%')) {
+        color_library_hsl.push([name, hue, saturation])
+    } else {
+        color_library_hsl.push([name, hue, saturation + '%'])
+    }
+}
+
+window.addEventListener("load", async () => {
+    let loaders = document.getElementsByClassName('loader')
+    if (loaders.length > 0) {
+        for (let i = 0; i < loaders.length; i++) {
+            loaders[i].style.opacity = '0';
+            await sleep(100);
+            loaders[i].style.display = 'none';
+        }
+    }
+})
+
+const sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 window.onload = () => {
+    console.log(
+        "%cThank you for using PixelbiteCSS :)",
+        "font-size:18px;font-weight:bold"
+    );
+    console.log(
+        "%cMore information or documentation can be found on our GitHub\nhttps://github.com/Pixelbite-CSS",
+        "font-weight:bold"
+    );
+    addSignature()
     const elements = document.getElementsByTagName('*')
     for (let i = 0; i < elements.length; i++) {
         let element = elements[i]
@@ -51,9 +113,20 @@ const classSplitToString = (array, startPosition) => {
                     array[i] = color_library[j][1]
                 }
             }
+            for (let j = 0; j < color_library_hsl.length; j++) {
+                if (array[i].includes(color_library_hsl[j][0])) {
+                    array[i] = array[i].replace(color_library_hsl[j][0], "hsl(" + color_library_hsl[j][1] + "," + color_library_hsl[j][2] + ",")
+                    array[i] += "%)"
+                }
+            }
             a += array[i] + " "
         }
         return a.trim()
     }
     else return ""
+}
+
+const addSignature = () => {
+    const comment = document.createComment("This site is created via PixelbiteCSS (https://github.com/Pixelbite-CSS)");
+    document.body.appendChild(comment);
 }
